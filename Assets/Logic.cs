71b47437837dfd68627507e31dd73f71c4b89d5e
Gustavo Blanco Alfaro <32990052+Tavo506@users.noreply.github.com>
2020-02-20 23:00:51 -0600

    Movimiento de camara y tablas

--- Assets/Logic.cs
+++ Assets/Logic.cs
@@ -7,8 +7,13 @@ using Random = UnityEngine.Random;
 
 public class Logic : MonoBehaviour
 {
+    public Canvas canvas;
+    public Camera camara1, camara2;
+    public Image tablaQuick, tablaBubble;
 
-    private int LIM = 500;
+    private static int LIM = 5000, START = 500;
+    public ArrayList tiemposQuick, tiemposBubble;
+
     private void QuickSort(int[] arr, int start, int end)
     {
         int i;
@@ -46,9 +51,10 @@ public class Logic : MonoBehaviour
 
     public void Quick()     //http://csharpexamples.com/c-quick-sort-algorithm-implementation/
     {
-        
-        for (int n = 2; n <= LIM; n++)
+        tiemposQuick = new ArrayList();
+        for (int n = START; n <= LIM; n += 250)
         {
+            
             int[] arr = new int[n];
             for (int i = 0; i < n; i++)
             {
@@ -57,7 +63,9 @@ public class Logic : MonoBehaviour
             System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
             sw.Start();
             QuickSort(arr, 0, arr.Length - 1);
-            Debug.Log(sw.Elapsed.TotalMilliseconds);
+            double tiempo = sw.Elapsed.TotalMilliseconds;
+            Debug.Log(tiempo);
+            tiemposQuick.Add(tiempo);
 
             //for (int i = 0; i < arr.Length; i++)
             //Debug.Log(arr[i]);
@@ -67,8 +75,9 @@ public class Logic : MonoBehaviour
 
     public void Bubble()    //https://www.w3resource.com/csharp-exercises/searching-and-sorting-algorithm/searching-and-sorting-algorithm-exercise-3.php
     {
+        tiemposBubble = new ArrayList();
         int t;
-        for (int n = 2; n <= LIM; n++) {
+        for (int n = START; n <= LIM; n += 250) {
             int[] a = new int[n];
             for (int i = 0; i < n; i++)
             {
@@ -88,7 +97,9 @@ public class Logic : MonoBehaviour
                     }
                 }
             }
-            Debug.Log(sw.Elapsed.TotalMilliseconds);
+            double tiempo = sw.Elapsed.TotalMilliseconds;
+            Debug.Log(tiempo);
+            tiemposBubble.Add(tiempo);
             //foreach (int aa in a)
             //Debug.Log(aa + " ");
             Debug.Log("--------------" + n + "--------------");
@@ -96,5 +107,31 @@ public class Logic : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)){
+            canvas.enabled = true;
+            camara1.enabled = true;
+            camara2.enabled = false;
+        }
+    }
 
+    public void Iniciar()
+    {
+        Quick();
+        Bubble();
+
+        colocarQuick();
+    }
+
+    void colocarQuick()
+    {
+        GameObject esfera = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        esfera.transform.SetParent(tablaQuick.transform);
+        
+        esfera.transform.localScale = new Vector3(10,10,10);
+        esfera.transform.position = new Vector3(tablaQuick.transform.position.x- 564, tablaQuick.transform.position.y- 253, tablaQuick.transform.position.z);
+        
+    }
+    //-564 -253
 }
---
+++ Assets/Movimientos.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimientos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public int Speed = 5;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - Speed, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + Speed, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + Speed, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - Speed, transform.position.z);
+        }
+    }
+}
